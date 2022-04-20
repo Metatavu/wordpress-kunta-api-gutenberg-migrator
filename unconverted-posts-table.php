cb8fd1ec1d5aeb52ba970e9844dd6734b6b584c7
Simeon Platonov <simeon.platonov@gmail.com>
2022-04-20 18:16:28 +0300

    Return from testing to normal

--- unconverted-posts-table.php
+++ unconverted-posts-table.php
@@ -96,6 +96,8 @@ class KuntaApiMigratorUnconvertedPostsTable extends WP_List_Table {
       'orderby' => $_REQUEST['orderby'],
       'order' => $_REQUEST['order'],
       's' => $_REQUEST['s']
+			'meta_key' => 'kunta_api_guttenberg_migrator_status',
+			'meta_value' => 'not_migrated'
     ]);
 
 		$results = array();
